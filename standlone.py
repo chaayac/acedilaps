e7ab5cb5a7ffbecdbf46b46bb24e2e3f1ec39922
Christopher Chaaya <z5016114@student.unsw.edu.au>
2015-12-07 09:34:34 +1100

    kk

--- standlone.py
+++ standlone.py
@@ -9,7 +9,7 @@ import cgitb; cgitb.enable()  ## This line enables CGI error reporting
  
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
-server_address = ("", $PORT)
+server_address = ("", 8000)
 handler.cgi_directories = ["/"]
  
 httpd = server(server_address, handler)
